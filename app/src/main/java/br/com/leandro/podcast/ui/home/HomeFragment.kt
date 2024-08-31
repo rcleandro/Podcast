7458aa88091c67275483dab0e1dfc0cefae66630
Leandro Rodrigues de Carvalho <sj.leandro@gmail.com>
2024-08-30 22:55:19 -0300

    Update home views

--- app/src/main/java/br/com/leandro/podcast/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/leandro/podcast/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import br.com.leandro.podcast.databinding.FragmentHomeBinding
@@ -23,10 +22,6 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
