a7c82f57c3982de770c0c2f72c9b56443e880a46
Leandro Rodrigues de Carvalho <sj.leandro@gmail.com>
2024-09-02 14:34:31 -0300

    Update navigation rules

--- app/src/main/java/br/com/leandro/podcast/MainActivity.kt
+++ app/src/main/java/br/com/leandro/podcast/MainActivity.kt
@@ -1,9 +1,17 @@
 package br.com.leandro.podcast
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -19,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_details, R.id.navigation_player
@@ -39,20 +48,65 @@ class MainActivity : AppCompatActivity() {
 
         navView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.navigation_home -> {
-                    navController.navigate(R.id.navigation_home)
-                    true
-                }
-                R.id.navigation_details -> {
-                    navController.navigate(R.id.navigation_details)
-                    true
-                }
-                R.id.navigation_player -> {
-                    navController.navigate(R.id.navigation_player)
-                    true
-                }
+                R.id.navigation_home -> navigateToHome()
+                R.id.navigation_details -> navigateToDetails()
+                R.id.navigation_player -> navigateToPlayer()
                 else -> false
             }
+        }
+    }
+
+    private fun navigateToHome(): Boolean {
+        navController.navigate(R.id.navigation_home)
+        return true
+    }
+
+    private fun navigateToDetails(): Boolean {
+        return viewModel.feed.value?.let {
+            navController.navigate(R.id.navigation_details)
+            true
+        } ?: run {
+            onShowDialog(getString(R.string.select_an_rss_link))
+            false
+        }
+    }
+
+    private fun navigateToPlayer(): Boolean {
+        return when {
+            viewModel.feed.value == null -> {
+                onShowDialog(getString(R.string.select_an_rss_link))
+                false
+            }
+            viewModel.podcast.value == null -> {
+                onShowDialog(getString(R.string.select_a_podcast_chapter))
+                false
+            }
+            else -> {
+                navController.navigate(R.id.navigation_player)
+                true
+            }
+        }
+    }
+
+    private fun onShowDialog(text: String) {
+        val alert = Dialog(this, R.style.dialogTheme)
+        alert.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        alert.setContentView(R.layout.dialog_view)
+        this.window?.setLayout(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+
+        val textView: TextView = alert.findViewById(R.id.text)
+        val buttonOk: Button = alert.findViewById(R.id.buttonOk)
+
+        textView.text = text
+
+        alert.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        alert.show()
+
+        buttonOk.setOnClickListener {
+            alert.dismiss()
         }
     }
 }
